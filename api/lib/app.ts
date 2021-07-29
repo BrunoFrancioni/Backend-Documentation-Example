b67db3dca933cfb6be55868d73edebe19dbb9a2d
Bruno Francioni <brunofrancioni1998@gmail.com>
2021-07-29 19:29:37 -0300

    App configuration and axios configured

--- api/lib/app.ts
+++ api/lib/app.ts
@@ -1,14 +1,22 @@
-import express from "express";
-
+import express, { Request, Response, NextFunction } from "express";
+import PhotosRoutes from "./routes/PhotosRoutes";
+import PostsRoutes from "./routes/PostsRoutes";
 class App {
     public app: express.Application;
 
+    public postsRoutes: PostsRoutes = new PostsRoutes();
+    public photosRoutes: PhotosRoutes = new PhotosRoutes();
+
     constructor() {
         this.app = express();
+        this.config();
+
+        this.postsRoutes.routes(this.app);
+        this.photosRoutes.routes(this.app);
     }
 
     private config(): void {
-        this.app.use((req, res, next) => {
+        this.app.use((req: Request, res: Response, next: NextFunction) => {
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
